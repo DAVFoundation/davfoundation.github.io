03258ca129cbc70f5ae82ebc2551db2867363615
Alex Iliescu <iliescualexandrurobert@gmail.com>
2017-12-15 21:45:31 +0200

    Made dropdown open on hover

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.dropdown-toggle').click(function(e) {
+  if ($(document).width() > 768) {
+    e.preventDefault();
+
+    var url = $(this).attr('href');
+
+       
+    if (url !== '#') {
+    
+      window.location.href = url;
+    }
+
+  }
+});
