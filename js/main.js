3bbd8993cd7860eb8f99fe1669f3eda448f18ea4
Tal Ater <tal@talater.com>
2017-12-27 22:59:44 +0200

    Simplified main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 $('.dropdown-toggle').click(function(e) {
   if ($(document).width() > 768) {
-    e.preventDefault();
-
     var url = $(this).attr('href');
-
-       
-    if (url !== '#') {
-    
-      window.location.href = url;
+    if (url === '#') {
+      e.preventDefault();
     }
-
   }
 });
