eb356b7bca1e9f6d32f646828edc7b98187856ea
Alex Iliescu <iliescualexandrurobert@gmail.com>
2018-02-02 20:58:42 +0200

    Updated new homepage 20180202

--- new-site/js/main.js
+++ new-site/js/main.js
@@ -1,15 +1,48 @@
-$(document).ready(function(){       
+$(document).ready(function(){   
+  // color switch for nav    
    var scroll_start = 0;
    var startchange = $('#startchange');
    var offset = startchange.offset();
     if (startchange.length){
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $(".navbar-fixed-top").addClass('user-scroll');
-       } else {
-          $(".navbar-fixed-top").removeClass('user-scroll');
-       }
-   });
+     $(document).scroll(function() { 
+  
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
+            $(".navbar-fixed-top").addClass('user-scroll');
+         } else {
+            $(".navbar-fixed-top").removeClass('user-scroll');
+         }
+     });
     }
+
+    // full height hero
+    var windowHeight = $(window).height();
+    $('.hero .hero-inner').height(windowHeight);
+    // $('.hero .hero-inner').height(windowHeight + windowHeight*0.18);
+
+    $(window).on('resize',function() { 
+      var windowHeight = $(window).height();
+      $('.hero .hero-inner').height(windowHeight);
+    });
+
+    // modal vertical align
+    $('.team-modal').on('shown.bs.modal', function (e) {
+      // alert("Modal for " + $(this).attr('id') + " is open.")
+      var windowHeight = $(window).height();
+      var modalHeight = $(this).find('.modal-content').height();
+      var modalTopPadding = (windowHeight - modalHeight)/2 - 30;
+      // var modalTopPadding = (windowHeight + windowHeight*0.18 - modalHeight)/2 - 30;
+      $('.modal-dialog').css('padding-top', modalTopPadding +'px');
+    })
+
+    // scroll nav
+    $(".nav-scroll").find("a").click(function(e) {
+      e.preventDefault();
+      var offset = $('.navbar').height();
+      // offset = offset + offset*0.15;
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top - offset
+      }, 700);
+    });
 });
