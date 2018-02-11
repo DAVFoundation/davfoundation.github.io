178222e1f32853d8876d88ada009b27eddca9df6
Tal Ater <tal@talater.com>
2018-02-11 12:22:21 +0200

    Removed old-site directory
    This is what version control systems are for :)

--- old-site/js/main.js
+++
@@ -1,8 +0,0 @@
-$('.dropdown-toggle').click(function(e) {
-  if ($(document).width() > 768) {
-    var url = $(this).attr('href');
-    if (url === '#') {
-      e.preventDefault();
-    }
-  }
-});
