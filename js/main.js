2aa35c6fabf961dece08930db59d0a70d5cae91e
Alex Iliescu <iliescualexandrurobert@gmail.com>
2018-02-19 19:06:00 +0200

    Announcement alert

--- js/main.js
+++ js/main.js
@@ -174,6 +174,43 @@ $(document).ready(function(){
         word.text("more");
       }
   });
+
+  // alert announcement
+
+    $('#alert-announcement').on('closed.bs.alert', function () {
+        setCookie('alert-dav', true, 365);
+        return false;
+    })
+    
+    function getCookie(c_name) {
+        var c_value = document.cookie;
+        var c_start = c_value.indexOf(" " + c_name + "=");
+        if (c_start == -1) {
+            c_start = c_value.indexOf(c_name + "=");
+        }
+        if (c_start == -1) {
+            c_value = null;
+        } else {
+            c_start = c_value.indexOf("=", c_start) + 1;
+            var c_end = c_value.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = c_value.length;
+            }
+            c_value = unescape(c_value.substring(c_start, c_end));
+        }
+        return c_value;
+    }
+
+    function setCookie(c_name, value, exdays) {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + exdays);
+        var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+        document.cookie = c_name + "=" + c_value;
+    }
+
+    if (getCookie('alert-dav') === "true") {
+       $('#alert-announcement').hide();
+    }
     
   // media galley
   $('.slick-gallery').slick({
