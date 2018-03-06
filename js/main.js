f092d604e5ec6139d3b34e829fa656ba78534f60
Alex Iliescu <iliescualexandrurobert@gmail.com>
2018-03-06 11:40:51 +0200

    iOS cursor fix

--- js/main.js
+++ js/main.js
@@ -15,6 +15,21 @@ $(document).ready(function(){
      });
     }
 
+    // iOS cursor fix
+    // Detect ios 11_x_x affected  
+    // NEED TO BE UPDATED if new versions are affected
+    var ua = navigator.userAgent,
+    iOS = /iPad|iPhone|iPod/.test(ua),
+    iOS11 = /OS 11_0|OS 11_1|OS 11_2/.test(ua);
+
+    // ios 11 bug caret position
+    if ( iOS && iOS11 ) {
+
+        // Add CSS class to body
+        $("body").addClass("iosBugFixCaret");
+
+    }
+
     // full height hero
     var windowHeight = $(window).height();
     $('.hero .hero-inner').height(windowHeight);
