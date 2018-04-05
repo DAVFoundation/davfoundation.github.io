6c2d274b92fd41336d545be99f216c06b3ff7f3c
Alex Iliescu <iliescualexandrurobert@gmail.com>
2018-04-05 20:23:16 +0300

    Fixes Homepage

--- js/main.js
+++ js/main.js
@@ -297,13 +297,24 @@ $(function(){
    }
   });
 // mailchimp subscriber count
-
+function addCommas(nStr)
+{
+  nStr += '';
+  x = nStr.split('.');
+  x1 = x[0];
+  x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
+}
 $.ajax({ 
     type: 'GET', 
     url: 'https://nessie.dav.network/members', 
     dataType: 'json',
     success: function (data) { 
-      $(".mailchimp-count").text(data.count);
+      $(".mailchimp-count").text(addCommas(data.count));
     }
 });
 // contributors section
