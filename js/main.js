316a6df9bf9fc0446425f4e9c2343efd08ab59aa
Tal Ater <tal@talater.com>
2018-04-08 10:14:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -311,7 +311,7 @@ $.ajax({
       $('.mailchimp-count').countTo({
         from: 16500,
         to: data.count,
-        speed: 3000,
+        speed: 1000,
         refreshInterval: 20,
         formatter: function (value, options) {
           value = value.toFixed(options.decimals);
