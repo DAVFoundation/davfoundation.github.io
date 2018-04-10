ba60ad97f40d3ca28a53ac375b25490f2957e6e2
Alex Iliescu <iliescualexandrurobert@gmail.com>
2018-04-10 20:21:27 +0300

    Added invalid email response for KYC status check

--- js/main.js
+++ js/main.js
@@ -314,6 +314,12 @@ $(document).ready(function(){
                         $(".kyc-response").text("We ask you to please resubmit your KYC by clicking the button below.");
                         $(".kyc-close,.kyc-medium,.kyc-telegram2").removeClass('hide');
                         break;
+                    case "Started":
+                        $(".kyc-title").text(" Your KYC application failed to process.");
+                        $(".kyc-response").text("Our systems tell us the email address you used is not valid. We ask that you please resubmit your KYC by clicking the button below and providing a valid email address.");
+                        $(".kyc-button,.kyc-medium,.kyc-telegram2").removeClass('hide');
+                        $(".kyc-button").attr("href","https://nessie.dav.network/join?email="+email);
+                        break;
                     default:
                       break;
                 }
