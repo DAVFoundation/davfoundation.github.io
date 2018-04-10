b7b3ab71a4b28e8ae8e9d170c5616eeedd803aba
Alex Iliescu <iliescualexandrurobert@gmail.com>
2018-04-10 21:23:59 +0300

    Made some text bold

--- js/main.js
+++ js/main.js
@@ -294,7 +294,7 @@ $(document).ready(function(){
                         break;
                     case "Failed":
                         $(".kyc-title").text("Your KYC application failed to process.");
-                        $(".kyc-response").html("We ask that you please resubmit your KYC by clicking the button below. Our systems tell us you should be able to successfully complete your KYC by doing the following:<br>" + data.suggestionText);
+                        $(".kyc-response").html("We ask that you please resubmit your KYC by clicking the button below. Our systems tell us you should be able to successfully complete your KYC by doing the following:<br><br><b>" + data.suggestionText + "</b>");
                         $(".kyc-button,.kyc-medium,.kyc-telegram2").removeClass('hide');
                         $(".kyc-button").attr("href","http://nessie.dav.network/join?email="+email);
                         break;
@@ -305,7 +305,7 @@ $(document).ready(function(){
                         break;
                     case "Rejected":
                         $(".kyc-title").text("Your KYC application has not been accepted.");
-                        $(".kyc-response").html("If you believe your KYC has been rejected by mistake we ask that you please resubmit your KYC by clicking the button below. Our systems tell us you should be able to successfully complete your KYC by doing the following:<br>" + data.suggestionText);
+                        $(".kyc-response").html("If you believe your KYC has been rejected by mistake we ask that you please resubmit your KYC by clicking the button below. Our systems tell us you should be able to successfully complete your KYC by doing the following:<br><br><b>" + data.suggestionText + "</b>");
                         $(".kyc-button,.kyc-medium,.kyc-telegram2").removeClass('hide');
                         $(".kyc-button").attr("href","https://nessie.dav.network/join?email="+email);
                         break;
