1b62251c3daef51fb0c52e5467efccc99df22917
Hai <hai.alaluf@gmail.com>
2018-06-06 15:34:36 +0300

    change hero for ico

--- js/main.js
+++ js/main.js
@@ -429,7 +429,7 @@ $(document).ready(function(){
     });
     //countdown
 
-    var targetDate=moment.utc([2018,5,11,13,0,0]);
+    var targetDate=moment.utc([2018,5,25,13,0,0]);
     $('#countdown,#countdownMobile').countdown({until: targetDate.toDate(), format: 'dHMS'});
 
   // register event google analytics
